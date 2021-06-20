e632034e485b4ad277467e1ee8c980c0d6c71ac4
donorp <donorp@arhat.solutions>
2021-06-20 19:50:50 +0200

    feat: Add system hardware arch detection utils
    Signed-off-by: donorp <donorp@arhat.solutions>

---
+++ pkg/constant/arch.go
@@ -0,0 +1,21 @@
+package constant
+
+const (
+	ArchX86   = "x86"
+	ArchAMD64 = "amd64"
+
+	ArchARM64 = "arm64"
+	ArchARMv5 = "armv5"
+	ArchARMv6 = "armv6"
+	ArchARMv7 = "armv7"
+
+	ArchMIPS     = "mips"
+	ArchMIPS64   = "mips64"
+	ArchMIPS64LE = "mips64le"
+
+	ArchPPC     = "ppc"
+	ArchPPC64   = "ppc64"
+	ArchPPC64LE = "ppc64le"
+
+	ArchIA64 = "ia64"
+)
