bc3e62bb9da91964d5eee19923879c410545d725
donorp <donorp@arhat.solutions>
2021-06-25 22:53:31 +0200

    tools(docker): Support dockerhub arch and sub command customization
    Signed-off-by: donorp <donorp@arhat.solutions>

--- cicd/docker/dukkha.linux.mips64le.dockerfile
+++ cicd/docker/dukkha.linux.mips64le.dockerfile
@@ -1,8 +1,7 @@
 ARG ARCH=amd64
-ARG HOST_PLATFORM_ARCH=amd64
 
-FROM --platform=linux/${HOST_PLATFORM_ARCH} ghcr.io/arhat-dev/builder-go:alpine as builder
-FROM --platform=linux/mips64le ghcr.io/arhat-dev/go:debian-mips64le
+FROM ghcr.io/arhat-dev/builder-go:alpine as builder
+FROM ghcr.io/arhat-dev/go:debian-mips64le
 ARG APP=dukkha
 
 LABEL org.opencontainers.image.source https://github.com/arhat-dev/dukkha
--- pkg/constant/arch_docker.go
+++ pkg/constant/arch_docker.go
@@ -13,3 +13,37 @@ func GetDockerArchVariant(mArch string) string {
 		ARCH_ARM64: "v8",
 	}[mArch]
 }
+
+func GetDockerHubArch(mArch string, other ...string) string {
+	mKernel := KERNEL_LINUX
+	if len(other) != 0 {
+		mKernel = other[0]
+	}
+
+	arch := map[string]string{
+		ARCH_AMD64: "amd64",
+		ARCH_X86:   "i386",
+
+		ARCH_ARM_V5: "arm32v5",
+		ARCH_ARM_V6: "arm32v6",
+		ARCH_ARM_V7: "arm32v7",
+
+		ARCH_ARM64: "arm64v8",
+
+		ARCH_MIPS64_LE:    "mips64le",
+		ARCH_MIPS64_LE_HF: "mips64le",
+
+		ARCH_PPC64_LE: "ppc64le",
+
+		ARCH_S390X: "s390x",
+	}[mArch]
+
+	switch mKernel {
+	case KERNEL_LINUX:
+		return arch
+	case KERNEL_WINDOWS:
+		return "win" + arch
+	default:
+		return arch
+	}
+}
