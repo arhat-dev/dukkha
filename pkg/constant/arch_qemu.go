68f3608d20a1b6a5b5bc133e6aab7e98bac6edcb
Jeffrey Stoke <me@arhat.dev>
2021-06-26 13:05:52 +0200

    Add qemu arch values
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

---
+++ pkg/constant/arch_qemu.go
@@ -0,0 +1,31 @@
+package constant
+
+func GetQemuArch(mArch string) string {
+	return map[string]string{
+		ARCH_X86:   "i386",
+		ARCH_AMD64: "x86_64",
+
+		ARCH_ARM64: "aarch64",
+
+		ARCH_ARM_V5: "arm",
+		ARCH_ARM_V6: "arm",
+		ARCH_ARM_V7: "arm",
+
+		ARCH_MIPS:       "mips",
+		ARCH_MIPS_HF:    "mips",
+		ARCH_MIPS_LE:    "mipsel",
+		ARCH_MIPS_LE_HF: "mipsel",
+
+		ARCH_MIPS64:       "mips64",
+		ARCH_MIPS64_HF:    "mips64",
+		ARCH_MIPS64_LE:    "mips64el",
+		ARCH_MIPS64_LE_HF: "mips64el",
+
+		ARCH_PPC:      "ppc",
+		ARCH_PPC64:    "ppc64",
+		ARCH_PPC64_LE: "ppc64le",
+
+		ARCH_S390X:    "s390x",
+		ARCH_RISCV_64: "riscv64",
+	}[mArch]
+}
