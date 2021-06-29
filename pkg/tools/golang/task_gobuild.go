9a73a0b7b504ccccd4581c16f3e60c8b6d41f37b
Jeffrey Stoke <me@arhat.dev>
2021-06-29 07:00:47 +0200

    tools(golang): Update go build cmd generation
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- pkg/tools/golang/task_gobuild.go
+++ pkg/tools/golang/task_gobuild.go
@@ -41,86 +41,73 @@ type TaskBuild struct {
 	ExtraArgs []string `yaml:"extraArgs"`
 	Outputs   []string `yaml:"outputs"`
 
-	CGO struct {
-		Enabled bool     `yaml:"enabled"`
-		CFlags  []string `yaml:"cflags"`
-		LDFlags []string `yaml:"ldflags"`
+	CGO CGOSepc `yaml:"cgo"`
+}
 
-		HostCC  string `yaml:"hostCC"`
-		HostCXX string `yaml:"hostCXX"`
+type CGOSepc struct {
+	field.BaseField
 
-		TargetCC  string `yaml:"targetCC"`
-		TargetCXX string `yaml:"targetCXX"`
-	} `yaml:"cgo"`
+	Enabled bool     `yaml:"enabled"`
+	CFlags  []string `yaml:"cflags"`
+	LDFlags []string `yaml:"ldflags"`
+
+	HostCC  string `yaml:"hostCC"`
+	HostCXX string `yaml:"hostCXX"`
+
+	TargetCC  string `yaml:"targetCC"`
+	TargetCXX string `yaml:"targetCXX"`
 }
 
 func (c *TaskBuild) ToolKind() string { return ToolKind }
 func (c *TaskBuild) TaskKind() string { return TaskKindBuild }
 
 func (c *TaskBuild) GetExecSpecs(ctx *field.RenderingContext, toolCmd []string) ([]tools.TaskExecSpec, error) {
+	mKernel := ctx.Values().Env[constant.ENV_MATRIX_KERNEL]
+	mArch := ctx.Values().Env[constant.ENV_MATRIX_ARCH]
+	doingCrossCompiling := ctx.Values().Env[constant.ENV_HOST_KERNEL] != mKernel ||
+		ctx.Values().Env[constant.ENV_HOST_ARCH] != mArch
+
+	env := sliceutils.NewStringSlice(c.Env, c.CGO.getEnv(
+		doingCrossCompiling, mKernel, mArch,
+		ctx.Values().Env[constant.ENV_HOST_OS],
+		ctx.Values().Env[constant.ENV_MATRIX_LIBC],
+	)...)
+
+	env = append(env, "GOOS="+constant.GetGolangOS(mKernel))
+	env = append(env, "GOARCH="+constant.GetGolangArch(mArch))
+
+	if envGOMIPS := c.getGOMIPS(mArch); len(envGOMIPS) != 0 {
+		env = append(env, "GOMIPS="+envGOMIPS)
+	}
+
+	if envGOARM := c.getGOARM(mArch); len(envGOARM) != 0 {
+		env = append(env, "GOARM="+envGOARM)
+	}
+
 	outputs := c.Outputs
 	if len(outputs) == 0 {
 		outputs = []string{c.Name}
 	}
 
-	var ret []tools.TaskExecSpec
+	var buildSteps []tools.TaskExecSpec
 	for _, output := range outputs {
-		spec := &tools.TaskExecSpec{}
-
-		spec.Env = append(spec.Env, c.Env...)
-
-		if c.CGO.Enabled {
-			spec.Env = append(spec.Env, "CGO_ENABLED=1")
-
-			if len(c.CGO.CFlags) != 0 {
-				spec.Env = append(spec.Env, fmt.Sprintf("CGO_CFLAGS=%s", strings.Join(c.CGO.CFlags, " ")))
-			}
-
-			if len(c.CGO.LDFlags) != 0 {
-				spec.Env = append(spec.Env, fmt.Sprintf("CGO_LDFLAGS=%s", strings.Join(c.CGO.LDFlags, " ")))
-			}
-
-			if len(c.CGO.HostCC) != 0 {
-				spec.Env = append(spec.Env, "CC="+c.CGO.HostCC)
-			}
-
-			if len(c.CGO.HostCXX) != 0 {
-				spec.Env = append(spec.Env, "CXX="+c.CGO.HostCXX)
-			}
-
-			if len(c.CGO.TargetCC) != 0 {
-				spec.Env = append(spec.Env, "CC_FOR_TARGET="+c.CGO.TargetCC)
-			}
-
-			if len(c.CGO.TargetCXX) != 0 {
-				spec.Env = append(spec.Env, "CXX_FOR_TARGET="+c.CGO.TargetCXX)
-			}
-		} else {
-			spec.Env = append(spec.Env, "CGO_ENABLED=0")
-		}
-
-		envGOOS := c.getGOOS(ctx.Values().Env[constant.ENV_MATRIX_KERNEL])
-		spec.Env = append(spec.Env, "GOOS="+envGOOS)
-
-		mArch := ctx.Values().Env[constant.ENV_MATRIX_ARCH]
-		spec.Env = append(spec.Env, "GOARCH="+constant.GetGolangArch(mArch))
-
-		switch {
-		case strings.HasPrefix(mArch, "mips"):
-			spec.Env = append(spec.Env, "GOMIPS="+c.getGOMIPS(mArch))
-		case strings.HasPrefix(mArch, "armv"):
-			spec.Env = append(spec.Env, "GOARM="+c.getGOARM(mArch))
+		spec := &tools.TaskExecSpec{
+			Env:     sliceutils.NewStringSlice(env),
+			Command: sliceutils.NewStringSlice(toolCmd, "build", "-o", output),
 		}
 
 		spec.Command = append(spec.Command, c.ExtraArgs...)
-		spec.Command = sliceutils.NewStringSlice(toolCmd, "build", "-o", output)
 
 		if len(c.LDFlags) != 0 {
-			spec.Command = append(spec.Command, fmt.Sprintf("-ldflags=\"%s\"", strings.Join(c.LDFlags, " ")))
+			spec.Command = append(spec.Command,
+				fmt.Sprintf("-ldflags=\"%s\"", strings.Join(c.LDFlags, " ")),
+			)
 		}
 
 		if len(c.Tags) != 0 {
-			spec.Command = append(spec.Command, fmt.Sprintf("-tags=\"%s\"", strings.Join(c.Tags, " ")))
+			spec.Command = append(spec.Command,
+				fmt.Sprintf("-tags=\"%s\"", strings.Join(c.Tags, " ")),
+			)
 		}
 
 		if len(c.Path) != 0 {
@@ -129,24 +116,97 @@ func (c *TaskBuild) GetExecSpecs(ctx *field.RenderingContext, toolCmd []string) 
 			spec.Command = append(spec.Command, ".")
 		}
 
-		ret = append(ret, *spec)
+		buildSteps = append(buildSteps, *spec)
 	}
 
-	return ret, nil
-}
-
-func (c *TaskBuild) getGOOS(mKernel string) string {
-	return mKernel
+	return buildSteps, nil
 }
 
 func (c *TaskBuild) getGOARM(mArch string) string {
-	return strings.TrimPrefix(mArch, "armv")
+	if strings.HasPrefix(mArch, "armv") {
+		return strings.TrimPrefix(mArch, "armv")
+	}
+
+	return ""
 }
 
 func (c *TaskBuild) getGOMIPS(mArch string) string {
+	if !strings.HasPrefix(mArch, "mips") {
+		return ""
+	}
+
 	if strings.HasSuffix(mArch, "hf") {
 		return "hardfloat"
 	}
 
 	return "softfloat"
+}
+
+func (c *CGOSepc) getEnv(doingCrossCompiling bool, mKernel, mArch, hostOS, targetLibc string) []string {
+	if !c.Enabled {
+		return []string{"CGO_ENABLED=0"}
+	}
+
+	var ret []string
+	ret = append(ret, "CGO_ENABLED=1")
+
+	if len(c.CFlags) != 0 {
+		ret = append(ret, fmt.Sprintf("CGO_CFLAGS=%s", strings.Join(c.CFlags, " ")))
+	}
+
+	if len(c.LDFlags) != 0 {
+		ret = append(ret, fmt.Sprintf("CGO_LDFLAGS=%s", strings.Join(c.LDFlags, " ")))
+	}
+
+	if len(c.HostCC) != 0 {
+		ret = append(ret, "CC="+c.HostCC)
+	}
+
+	if len(c.HostCXX) != 0 {
+		ret = append(ret, "CXX="+c.HostCXX)
+	}
+
+	targetCC := "gcc"
+	targetCXX := "g++"
+	if doingCrossCompiling {
+		switch hostOS {
+		case constant.OS_DEBIAN,
+			constant.OS_UBUNTU:
+			var tripleName string
+			switch mKernel {
+			case constant.KERNEL_LINUX:
+				tripleName = constant.GetDebianTripleName(mArch, mKernel, targetLibc)
+			case constant.KERNEL_DARWIN:
+				// TODO: set darwin version
+				tripleName = constant.GetDarwinTripleName(mArch, "")
+			case constant.KERNEL_WINDOWS:
+				tripleName = constant.GetDebianTripleName(mArch, mKernel, targetLibc)
+			default:
+			}
+
+			targetCC = tripleName + "-gcc"
+			targetCXX = tripleName + "-g++"
+		case constant.OS_ALPINE:
+			tripleName := constant.GetAlpineTripleName(mArch)
+			targetCC = tripleName + "-gcc"
+			targetCXX = tripleName + "-g++"
+		case constant.OS_MACOS:
+			targetCC = "clang"
+			targetCXX = "clang++"
+		}
+	}
+
+	if len(c.TargetCC) != 0 {
+		ret = append(ret, "CC_FOR_TARGET="+c.TargetCC)
+	} else if doingCrossCompiling {
+		ret = append(ret, "CC_FOR_TARGET="+targetCC)
+	}
+
+	if len(c.TargetCXX) != 0 {
+		ret = append(ret, "CXX_FOR_TARGET="+c.TargetCXX)
+	} else if doingCrossCompiling {
+		ret = append(ret, "CC_FOR_TARGET="+targetCXX)
+	}
+
+	return ret
 }
