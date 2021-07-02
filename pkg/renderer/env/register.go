8a34c64f722b0bc5a4a2ded2b527b31cf93cd4e7
Jeffrey Stoke <me@arhat.dev>
2021-07-02 07:25:55 +0200

    Add renderer `env`
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

---
+++ pkg/renderer/env/register.go
@@ -0,0 +1,22 @@
+package env
+
+import (
+	"fmt"
+
+	"arhat.dev/dukkha/pkg/renderer"
+)
+
+func init() {
+	renderer.Register(&Config{}, NewDriver)
+}
+
+func NewDriver(config interface{}) (renderer.Interface, error) {
+	cfg, ok := config.(*Config)
+	if !ok {
+		return nil, fmt.Errorf("unexpected non %s renderer config: %T", DefaultName, config)
+	}
+
+	_ = cfg
+
+	return &Driver{}, nil
+}
