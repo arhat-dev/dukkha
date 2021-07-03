786b4bb5ad95e4757ee67b7692a189745054940d
Jeffrey Stoke <me@arhat.dev>
2021-07-03 21:06:36 +0200

    feat(tools): Add github cli release support
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

---
+++ pkg/tools/github/task_release_test.go
@@ -0,0 +1,13 @@
+package github_test
+
+import (
+	"testing"
+
+	"arhat.dev/dukkha/pkg/tools/tests"
+)
+
+func TestTaskRelease_GetExecSpecs(t *testing.T) {
+	toolCmd := []string{"gh"}
+	testCases := []tests.ExecSpecGenerationTestCase{}
+	tests.RunTaskExecSpecGenerationTests(t, nil, toolCmd, testCases)
+}
