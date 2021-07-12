ab74f18315352eccbdcd55438c44c46e76bc2958
donorp <donorp@arhat.solutions>
2021-07-12 12:24:04 +0200

    feat(cmd): Support `include` in config file
    Close #55
    
    Signed-off-by: donorp <donorp@arhat.solutions>

--- pkg/cmd/config.go
+++ pkg/cmd/config.go
@@ -15,25 +15,53 @@ import (
 func readConfigRecursively(
 	configPaths []string,
 	ignoreFileNotExist bool,
+	visitedPaths *map[string]struct{},
 	mergedConfig *conf.Config,
 ) error {
-	readAndMergeConfigFile := func(path string) error {
-		configBytes, err2 := os.ReadFile(path)
-		if err2 != nil {
-			return fmt.Errorf("failed to read config file %q: %w", path, err2)
+	readAndMergeConfigFile := func(file string) error {
+		if _, ok := (*visitedPaths)[file]; ok {
+			return nil
+		}
+
+		(*visitedPaths)[file] = struct{}{}
+
+		configBytes, err := os.ReadFile(file)
+		if err != nil {
+			return fmt.Errorf("failed to read config file %q: %w", file, err)
 		}
 
 		current := conf.NewConfig()
-		err2 = yaml.Unmarshal(configBytes, &current)
-		if err2 != nil {
-			return fmt.Errorf("failed to unmarshal config file %q: %w", path, err2)
+		err = yaml.Unmarshal(configBytes, &current)
+		if err != nil {
+			return fmt.Errorf("failed to unmarshal config file %q: %w", file, err)
 		}
 
-		log.Log.V("config unmarshaled", log.String("file", path), log.Any("config", current))
+		log.Log.V("config unmarshaled", log.String("file", file), log.Any("config", current))
 
 		mergedConfig.Merge(current)
 
-		return err2
+		for _, inc := range current.Include {
+			log.Log.V("working on include entry", log.String("value", inc))
+
+			var toInclude string
+			if filepath.IsAbs(inc) {
+				toInclude = inc
+			} else {
+				toInclude = filepath.Join(filepath.Dir(file), inc)
+			}
+
+			matches, err2 := filepath.Glob(toInclude)
+			if err2 != nil {
+				matches = []string{toInclude}
+			}
+
+			err2 = readConfigRecursively(matches, false, visitedPaths, mergedConfig)
+			if err2 != nil {
+				return fmt.Errorf("failed to load included config files: %w", err)
+			}
+		}
+
+		return err
 	}
 
 	for _, path := range configPaths {
