76fda97ec9b88a7a1b21550ab2fea3fd194fe90c
donorp <donorp@arhat.solutions>
2021-08-19 11:16:00 +0200

    feat(renderer): Add s3 renderer
    Signed-off-by: donorp <donorp@arhat.solutions>

---
+++ pkg/renderer/s3/config.go
@@ -0,0 +1,82 @@
+package s3
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/url"
+	"path"
+
+	"arhat.dev/rs"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type inputS3Sepc struct {
+	rs.BaseField `yaml:"-"`
+
+	Path string `yaml:"path"`
+
+	Config rendererS3Config `yaml:"config"`
+}
+
+type rendererS3Config struct {
+	rs.BaseField `yaml:"-"`
+
+	EndpointURL string `yaml:"endpoint_url"`
+	Region      string `yaml:"region"`
+
+	Bucket   string `yaml:"bucket"`
+	BasePath string `yaml:"base_path"`
+
+	AccessKeyID     string `yaml:"access_key_id"`
+	AccessKeySecret string `yaml:"access_key_secret"`
+}
+
+type s3Client struct {
+	client *minio.Client
+
+	bucket   string
+	region   string
+	basePath string
+}
+
+func (c *s3Client) download(ctx context.Context, objPath string) ([]byte, error) {
+	obj, err := c.client.GetObject(
+		ctx,
+		c.bucket,
+		path.Join(c.basePath, objPath),
+		minio.GetObjectOptions{},
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = obj.Close() }()
+
+	data, err := io.ReadAll(obj)
+	return data, err
+}
+
+func (c *rendererS3Config) createClient() (*s3Client, error) {
+	eURL, err := url.Parse(c.EndpointURL)
+	if err != nil {
+		return nil, fmt.Errorf("invalid endpoint url: %w", err)
+	}
+
+	client, err := minio.New(eURL.Host, &minio.Options{
+		Creds:  credentials.NewStaticV4(c.AccessKeyID, c.AccessKeySecret, ""),
+		Secure: eURL.Scheme == "https",
+		Region: c.Region,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create s3 client: %w", err)
+	}
+
+	return &s3Client{
+		client: client,
+
+		bucket:   c.Bucket,
+		region:   c.Region,
+		basePath: c.BasePath,
+	}, nil
+}
