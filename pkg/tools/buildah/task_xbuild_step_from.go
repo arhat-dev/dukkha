e363dc0b9d198dc5c5a618f5060b14895b03596e
donorp <donorp@arhat.solutions>
2021-10-07 15:29:10 +0200

    feat(buildah): Implement xbuild steps
    Signed-off-by: donorp <donorp@arhat.solutions>

--- pkg/tools/buildah/task_xbuild_step_from.go
+++ pkg/tools/buildah/task_xbuild_step_from.go
@@ -1,127 +1,14 @@
 package buildah
 
 import (
-	"fmt"
+	"bytes"
+	"strings"
 
 	"arhat.dev/dukkha/pkg/constant"
 	"arhat.dev/dukkha/pkg/dukkha"
 	"arhat.dev/dukkha/pkg/sliceutils"
 	"arhat.dev/rs"
 )
-
-type stepFrom struct {
-	rs.BaseField
-
-	// Image as base rootfs
-	Image *fromImageSpec `yaml:"image"`
-
-	// Step as base rootfs
-	Step *fromStepSpec `yaml:"step"`
-
-	// TODO
-	Mount []*mountSpec `yaml:"mount"`
-}
-
-func (s *stepFrom) genSpec(
-	rc dukkha.TaskExecContext,
-	options dukkha.TaskMatrixExecOptions,
-	stepCtx *xbuildContext,
-) ([]dukkha.TaskExecSpec, error) {
-	switch {
-	case s.Image != nil:
-		return s.Image.genSpec(rc, options, stepCtx)
-	case s.Step != nil:
-		return s.Step.genSpec(rc, options, stepCtx)
-	default:
-		return nil, fmt.Errorf("invalid empty from spec")
-	}
-}
-
-type fromImageSpec struct {
-	rs.BaseField
-
-	// Name of the image
-	Name *string `yaml:"name"`
-	// Digest of the image
-	Digest *string `yaml:"digest"`
-
-	Kernel string `yaml:"kernel"`
-	Arch   string `yaml:"arch"`
-
-	AlwaysPull    bool     `yaml:"always_pull"`
-	NeverPull     bool     `yaml:"never_pull"`
-	ExtraPullArgs []string `yaml:"extra_pull_args"`
-}
-
-func (s *fromImageSpec) genSpec(
-	rc dukkha.TaskExecContext,
-	options dukkha.TaskMatrixExecOptions,
-	stepCtx *xbuildContext,
-) ([]dukkha.TaskExecSpec, error) {
-	// pull with os/arch/variant
-	//
-	// flag option --platform can override it
-	switch {
-	case s.AlwaysPull:
-		pullCmd := sliceutils.NewStrings(options.ToolCmd(), "pull")
-
-		if len(s.Kernel) != 0 {
-			os, ok := constant.GetOciOS(s.Kernel)
-			if !ok {
-				os = s.Kernel
-			}
-
-			pullCmd = append(pullCmd, "--os", os)
-		}
-
-		if len(s.Arch) != 0 {
-			arch, ok := constant.GetOciArch(s.Arch)
-			if !ok {
-				arch = s.Arch
-			}
-			pullCmd = append(pullCmd, "--arch", arch)
-
-			variant, ok := constant.GetOciArchVariant(s.Arch)
-			if ok {
-				pullCmd = append(pullCmd, "--variant", variant)
-			}
-		}
-
-		pullCmd = append(pullCmd, s.ExtraPullArgs...)
-	case s.NeverPull:
-	default:
-		// pull when image does not exist
-	}
-
-	fromCmd := sliceutils.NewStrings(options.ToolCmd(), "from", "--cidfile")
-
-	switch {
-	case s.Name != nil:
-		fromCmd = append(fromCmd, "docker://"+*s.Name)
-	case s.Digest != nil:
-		fromCmd = append(fromCmd, "")
-	default:
-		return nil, fmt.Errorf("invalid no image reference")
-	}
-
-	return nil, nil
-}
-
-type fromStepSpec struct {
-	rs.BaseField
-
-	ID string `yaml:"id"`
-}
-
-func (s *fromStepSpec) genSpec(
-	rc dukkha.TaskExecContext,
-	options dukkha.TaskMatrixExecOptions,
-	stepCtx *xbuildContext,
-) ([]dukkha.TaskExecSpec, error) {
-	refCtx := stepCtx.Steps[s.ID]
-	_ = refCtx
-	return nil, nil
-}
 
 type mountSpec struct {
 	From string `yaml:"from"`
@@ -138,4 +25,127 @@ type mountSpec struct {
 
 	// AsOverlay adds `O` option to the mount, build will mount it using overlayfs
 	AsOverlay bool
+}
+
+type stepFrom struct {
+	rs.BaseField
+
+	// Ref image
+	Ref string `yaml:"ref"`
+
+	Kernel string `yaml:"kernel"`
+	Arch   string `yaml:"arch"`
+
+	ExtraPullArgs []string `yaml:"extra_pull_args"`
+
+	// TODO: implement
+	Mount []mountSpec `yaml:"mount"`
+
+	ExtraArgs []string `yaml:"extra_args"`
+}
+
+func (s *stepFrom) genSpec(
+	rc dukkha.TaskExecContext,
+	options dukkha.TaskMatrixExecOptions,
+) ([]dukkha.TaskExecSpec, error) {
+	var (
+		platformArgs []string
+	)
+
+	if len(s.Kernel) != 0 {
+		os, ok := constant.GetOciOS(s.Kernel)
+		if !ok {
+			os = s.Kernel
+		}
+
+		platformArgs = append(platformArgs, "--os", os)
+	}
+
+	if len(s.Arch) != 0 {
+		arch, ok := constant.GetOciArch(s.Arch)
+		if !ok {
+			arch = s.Arch
+		}
+		platformArgs = append(platformArgs, "--arch", arch)
+
+		variant, ok := constant.GetOciArchVariant(s.Arch)
+		if ok {
+			platformArgs = append(platformArgs, "--variant", variant)
+		}
+	}
+
+	var steps []dukkha.TaskExecSpec
+
+	const (
+		replace_XBUILD_FROM_IMAGE_REF = "<XBUILD_FROM_IMAGE_REF>"
+	)
+
+	var imageRef = replace_XBUILD_FROM_IMAGE_REF
+	switch strings.ToLower(s.Ref) {
+	case "scratch":
+		imageRef = s.Ref
+	default:
+		// pull with os/arch/variant
+		//
+		// flag option --platform can override it
+
+		// buildah pull [--policy always|never|missing] [--os OCI_OS] [--arch OCI_ARCH] [--variant OCI_VARIANT]
+		pullCmd := sliceutils.NewStrings(options.ToolCmd(), "pull")
+
+		pullCmd = append(pullCmd, platformArgs...)
+		pullCmd = append(pullCmd, s.ExtraPullArgs...)
+		pullCmd = append(pullCmd, s.Ref)
+
+		steps = append(steps, dukkha.TaskExecSpec{
+			StdoutAsReplace:          replace_XBUILD_FROM_IMAGE_REF,
+			FixStdoutValueForReplace: bytes.TrimSpace,
+
+			ShowStdout:  true,
+			IgnoreError: false,
+			Command:     pullCmd,
+			UseShell:    options.UseShell(),
+			ShellName:   options.ShellName(),
+		})
+	}
+
+	fromCmd := sliceutils.NewStrings(options.ToolCmd(), "from")
+	fromCmd = append(fromCmd, platformArgs...)
+	fromCmd = append(fromCmd, s.ExtraArgs...)
+	fromCmd = append(fromCmd, imageRef)
+
+	const (
+		replace_XBUILD_CURRENT_CONTAINER_NAME = "<XBUILD_CURRENT_CONTAINER_NAME>"
+	)
+
+	// produce container name
+	steps = append(steps, dukkha.TaskExecSpec{
+		StdoutAsReplace:          replace_XBUILD_CURRENT_CONTAINER_NAME,
+		FixStdoutValueForReplace: bytes.TrimSpace,
+
+		ShowStdout:  true,
+		IgnoreError: false,
+		Command:     fromCmd,
+		UseShell:    options.UseShell(),
+		ShellName:   options.ShellName(),
+	})
+
+	// retrieve container id
+	steps = append(steps, dukkha.TaskExecSpec{
+		StdoutAsReplace:          replace_XBUILD_CURRENT_CONTAINER_ID,
+		FixStdoutValueForReplace: bytes.TrimSpace,
+
+		ShowStdout:  true,
+		IgnoreError: false,
+		Command: sliceutils.NewStrings(
+			options.ToolCmd(),
+			"inspect",
+			"--type", "container",
+			"--format", "{{ .ContainerID }}",
+			replace_XBUILD_CURRENT_CONTAINER_NAME,
+		),
+		UseShell:  options.UseShell(),
+		ShellName: options.ShellName(),
+	})
+
+	return steps, nil
 }
--- pkg/tools/buildah/task_xbuild_step_run.go
+++ pkg/tools/buildah/task_xbuild_step_run.go
@@ -1,24 +1,134 @@
 package buildah
 
 import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
 	"arhat.dev/dukkha/pkg/dukkha"
+	"arhat.dev/dukkha/pkg/sliceutils"
+	"arhat.dev/pkg/md5helper"
 	"arhat.dev/rs"
 )
 
 type stepRun struct {
 	rs.BaseField
 
-	Script *string  `yaml:"script"`
-	Cmd    []string `yaml:"cmd"`
+	// Script for shell execution
+	Script string `yaml:"script"`
+
+	Cmd []string `yaml:"cmd"`
 
 	// Shell overrides default shell
 	Shell []string `yaml:"shell"`
+
+	ExtraArgs []string `yaml:"extra_args"`
 }
 
 func (s *stepRun) genSpec(
 	rc dukkha.TaskExecContext,
 	options dukkha.TaskMatrixExecOptions,
-	stepCtx *xbuildContext,
+	record bool,
 ) ([]dukkha.TaskExecSpec, error) {
-	return nil, nil
+	runCmd := sliceutils.NewStrings(options.ToolCmd(), "run")
+	if record {
+		runCmd = append(runCmd, "--add-history")
+	}
+	runCmd = append(runCmd, s.ExtraArgs...)
+	runCmd = append(runCmd, replace_XBUILD_CURRENT_CONTAINER_ID, "--")
+
+	var steps []dukkha.TaskExecSpec
+
+	switch {
+	case len(s.Cmd) != 0:
+		steps = append(steps, dukkha.TaskExecSpec{
+			IgnoreError: false,
+			Command:     append(runCmd, s.Cmd...),
+			UseShell:    options.UseShell(),
+			ShellName:   options.ShellName(),
+		})
+	case len(s.Script) != 0:
+		// copy this script to container
+		const (
+			replace_XBUILD_RUN_SCRIPT_SRC_PATH          = "<XBUILD_RUN_SCRIPT_SRC_PATH>"
+			replace_XBUILD_RUN_SCRIPT_SRC_REDACTED_PATH = "<XBUILD_RUN_SCRIPT_SRC_REDACTED_PATH>"
+		)
+
+		script := s.Script
+		steps = append(steps,
+			// write script to local cache
+			dukkha.TaskExecSpec{
+				StdoutAsReplace:          replace_XBUILD_RUN_SCRIPT_SRC_PATH,
+				FixStdoutValueForReplace: bytes.TrimSpace,
+
+				ShowStdout: true,
+				AlterExecFunc: func(replace dukkha.ReplaceEntries, stdin io.Reader, stdout, stderr io.Writer) (dukkha.RunTaskOrRunCmd, error) {
+					srcFile := filepath.Join(rc.CacheDir(), "buildah", "xbuild", "run-script-"+hex.EncodeToString(md5helper.Sum([]byte(script))))
+					err := os.MkdirAll(filepath.Dir(srcFile), 0755)
+					if err != nil {
+						return nil, fmt.Errorf("failed to ensure script cache dir: %w", err)
+					}
+
+					_, err = stdout.Write([]byte(srcFile))
+					if err != nil {
+						return nil, fmt.Errorf("failed to create script cache: %q", srcFile)
+					}
+
+					return nil, os.WriteFile(srcFile, []byte(script), 0644)
+				},
+			},
+			// write redacted file
+			dukkha.TaskExecSpec{
+				StdoutAsReplace:          replace_XBUILD_RUN_SCRIPT_SRC_REDACTED_PATH,
+				FixStdoutValueForReplace: bytes.TrimSpace,
+
+				AlterExecFunc: func(replace dukkha.ReplaceEntries, stdin io.Reader, stdout, stderr io.Writer) (dukkha.RunTaskOrRunCmd, error) {
+					v, ok := replace[replace_XBUILD_RUN_SCRIPT_SRC_PATH]
+					if !ok {
+						return nil, fmt.Errorf("unexpected script path not found")
+					}
+
+					srcFile := string(v.Data)
+					redactedSrcFile := srcFile + "-redacted"
+					_, err := stdout.Write([]byte(redactedSrcFile))
+					if err != nil {
+						return nil, fmt.Errorf("failed to write redacted file path: %w", err)
+					}
+
+					return nil, os.WriteFile(redactedSrcFile, []byte(""), 0644)
+				},
+			},
+			// copy script to container
+			dukkha.TaskExecSpec{
+				Command: sliceutils.NewStrings(
+					options.ToolCmd(), "copy", "--chmod", "0755",
+					replace_XBUILD_CURRENT_CONTAINER_ID,
+					replace_XBUILD_RUN_SCRIPT_SRC_PATH, "/tmp/xbuild-script",
+				),
+			},
+			// run the script
+			dukkha.TaskExecSpec{
+				IgnoreError: false,
+				Command:     append(runCmd, "/tmp/xbuild-script"),
+				UseShell:    options.UseShell(),
+				ShellName:   options.ShellName(),
+			},
+			// override that script
+			dukkha.TaskExecSpec{
+				IgnoreError: false,
+				Command: sliceutils.NewStrings(
+					options.ToolCmd(), "copy", "--chmod", "0644",
+					replace_XBUILD_CURRENT_CONTAINER_ID,
+					replace_XBUILD_RUN_SCRIPT_SRC_REDACTED_PATH, "/tmp/xbuild-script",
+				),
+			},
+		)
+	default:
+		return nil, fmt.Errorf("invalid empty run statement")
+	}
+
+	return steps, nil
 }
