6796c2af9cb3207dbdb7665afa2543d701827866
donorp <donorp@arhat.solutions>
2021-10-09 15:32:04 +0200

    feat(buildah|xbuild): Add executable file as run option
    Signed-off-by: donorp <donorp@arhat.solutions>

--- pkg/tools/buildah/task_xbuild_step_run.go
+++ pkg/tools/buildah/task_xbuild_step_run.go
@@ -19,15 +19,36 @@ type stepRun struct {
 	rs.BaseField
 
 	// Script
+	//
+	// helpful when you need to run remote script, use:
+	//
+	// 	run:
+	// 	  script@http: http://some-script.company
+	//
 	Script string `yaml:"script"`
 
 	// ScriptArgs for the script
 	ScriptArgs []string `yaml:"script_args"`
 
+	// ExecutableFile path in local fs, run it in container
+	//
+	// Will copy the executable to container and remove it after executaion
+	//
+	// helpful when your executable is large to load as `script`
+	ExecutableFile string `yaml:"executable_file"`
+
+	// ExecutableArgs for the executable
+	ExecutableArgs []string `yaml:"executable_args"`
+
+	// Cmd as bare exec
 	Cmd []string `yaml:"cmd"`
 
 	ExtraArgs []string `yaml:"extra_args"`
 }
+
+const (
+	execPathInContainer = "/tmp/xbuild-run"
+)
 
 func (s *stepRun) genSpec(
 	rc dukkha.TaskExecContext,
@@ -51,6 +72,65 @@ func (s *stepRun) genSpec(
 			UseShell:    options.UseShell(),
 			ShellName:   options.ShellName(),
 		})
+	case len(s.ExecutableFile) != 0:
+		localExecutablePath := s.ExecutableFile
+
+		const (
+			replace_XBUILD_RUN_EXECUTABLE_SRC_REDACTED_PATH = "<XBUILD_RUN_EXECUTABLE_SRC_REDACTED_PATH>"
+		)
+		steps = append(steps,
+			// write redacted file
+			dukkha.TaskExecSpec{
+				StdoutAsReplace:          replace_XBUILD_RUN_EXECUTABLE_SRC_REDACTED_PATH,
+				FixStdoutValueForReplace: bytes.TrimSpace,
+
+				AlterExecFunc: func(
+					replace dukkha.ReplaceEntries,
+					stdin io.Reader,
+					stdout, stderr io.Writer,
+				) (dukkha.RunTaskOrRunCmd, error) {
+					srcFile := filepath.Join(
+						rc.CacheDir(),
+						"buildah", "xbuild",
+						"run-executable-"+hex.EncodeToString(md5helper.Sum([]byte(localExecutablePath)))+"-redacted",
+					)
+					err := os.MkdirAll(filepath.Dir(srcFile), 0755)
+					if err != nil && !os.IsExist(err) {
+						return nil, fmt.Errorf("failed to ensure redacted executable file cache dir: %w", err)
+					}
+
+					_, err = stdout.Write([]byte(srcFile))
+					if err != nil {
+						return nil, fmt.Errorf("failed to create redacted executable file cache: %q", srcFile)
+					}
+
+					return nil, os.WriteFile(srcFile, []byte(""), 0644)
+				},
+			},
+			// copy executable to container
+			dukkha.TaskExecSpec{
+				Command: sliceutils.NewStrings(
+					options.ToolCmd(), "copy", "--chmod", "0755",
+					replace_XBUILD_CURRENT_CONTAINER_ID,
+					localExecutablePath, execPathInContainer,
+				),
+			},
+			dukkha.TaskExecSpec{
+				IgnoreError: false,
+				Command:     append(append(runCmd, execPathInContainer), s.ExecutableArgs...),
+				UseShell:    options.UseShell(),
+				ShellName:   options.ShellName(),
+			},
+			// override that executable
+			dukkha.TaskExecSpec{
+				IgnoreError: false,
+				Command: sliceutils.NewStrings(
+					options.ToolCmd(), "copy", "--chmod", "0644",
+					replace_XBUILD_CURRENT_CONTAINER_ID,
+					replace_XBUILD_RUN_EXECUTABLE_SRC_REDACTED_PATH, execPathInContainer,
+				),
+			},
+		)
 	case len(s.Script) != 0:
 		// copy this script to container
 		const (
@@ -77,7 +157,7 @@ func (s *stepRun) genSpec(
 						"run-script-"+hex.EncodeToString(md5helper.Sum([]byte(script))),
 					)
 					err := os.MkdirAll(filepath.Dir(srcFile), 0755)
-					if err != nil {
+					if err != nil && !os.IsExist(err) {
 						return nil, fmt.Errorf("failed to ensure script cache dir: %w", err)
 					}
 
@@ -119,13 +199,13 @@ func (s *stepRun) genSpec(
 				Command: sliceutils.NewStrings(
 					options.ToolCmd(), "copy", "--chmod", "0755",
 					replace_XBUILD_CURRENT_CONTAINER_ID,
-					replace_XBUILD_RUN_SCRIPT_SRC_PATH, "/tmp/xbuild-script",
+					replace_XBUILD_RUN_SCRIPT_SRC_PATH, execPathInContainer,
 				),
 			},
 			// run the script
 			dukkha.TaskExecSpec{
 				IgnoreError: false,
-				Command:     append(append(runCmd, "/tmp/xbuild-script"), s.ScriptArgs...),
+				Command:     append(append(runCmd, execPathInContainer), s.ScriptArgs...),
 				UseShell:    options.UseShell(),
 				ShellName:   options.ShellName(),
 			},
@@ -135,7 +215,7 @@ func (s *stepRun) genSpec(
 				Command: sliceutils.NewStrings(
 					options.ToolCmd(), "copy", "--chmod", "0644",
 					replace_XBUILD_CURRENT_CONTAINER_ID,
-					replace_XBUILD_RUN_SCRIPT_SRC_REDACTED_PATH, "/tmp/xbuild-script",
+					replace_XBUILD_RUN_SCRIPT_SRC_REDACTED_PATH, execPathInContainer,
 				),
 			},
 		)
