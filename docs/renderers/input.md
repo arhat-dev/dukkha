5b29a9e20bab193ebc7ca281cad3e6cc8dc645f8
donorp <donorp@arhat.solutions>
2021-11-23 19:10:07 +0100

    feat(renderer|input): Add attr `use-spec` support
    Accept `hide_input` and `prompt` as input option
    
    Signed-off-by: donorp <donorp@arhat.solutions>

--- docs/renderers/input.md
+++ docs/renderers/input.md
@@ -4,7 +4,7 @@
 foo@input: "Please Enter Something: "
 ```
 
-Read user console input as field value.
+Read console input as value.
 
 ## Config Options
 
@@ -14,7 +14,11 @@ __NOTE:__ Configuration is required to activate this renderer.
 renderers:
   input:
     # hide user input (e.g. entering password)
-    hide: true
+    # defaults to false
+    hide_input: true
+
+    # input prompt message string
+    prompt: ""
 ```
 
 ## Supported value types
@@ -25,6 +29,18 @@ renderers:
 foo@input: Enter your mood
 ```
 
+- Valid Input Spec (only when applied with attribute `#use-spec`)
+
+```yaml
+foo@input#use-spec:
+  # defaults to renderer config's hide_input option
+  hide_input: true
+
+  # defaults to renderer config's prompt option
+  prompt: ""
+```
+
 ## Suggested Use Cases
 
-Force user interaction before task execution.
+- Force user interaction before task execution.
+- Interactive task automation.
--- pkg/renderer/input/input.go
+++ pkg/renderer/input/input.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"os"
 
+	"arhat.dev/pkg/iohelper"
 	"arhat.dev/pkg/yamlhelper"
 	"arhat.dev/rs"
 	"golang.org/x/term"
+	"gopkg.in/yaml.v3"
 
 	"arhat.dev/dukkha/pkg/dukkha"
+	"arhat.dev/dukkha/pkg/renderer"
 )
 
-// nolint:revive
 const (
 	DefaultName = "input"
 )
@@ -25,9 +27,9 @@ var _ dukkha.Renderer = (*Driver)(nil)
 type Driver struct {
 	rs.BaseField `yaml:"-"`
 
-	Hide bool `yaml:"hide"`
-
 	name string
+
+	Config configSpec `yaml:",inline"`
 }
 
 func (d *Driver) Init(ctx dukkha.ConfigResolvingContext) error {
@@ -35,7 +37,7 @@ func (d *Driver) Init(ctx dukkha.ConfigResolvingContext) error {
 }
 
 func (d *Driver) RenderYaml(
-	_ dukkha.RenderingContext, rawData interface{}, _ []dukkha.RendererAttribute,
+	_ dukkha.RenderingContext, rawData interface{}, attributes []dukkha.RendererAttribute,
 ) ([]byte, error) {
 	rawData, err := rs.NormalizeRawData(rawData)
 	if err != nil {
@@ -47,14 +49,46 @@ func (d *Driver) RenderYaml(
 		return nil, err
 	}
 
-	fmt.Print(string(promptBytes))
-	if d.Hide {
+	var useSpec bool
+	for _, attr := range attributes {
+		switch attr {
+		case renderer.AttrUseSpec:
+			useSpec = true
+		default:
+		}
+	}
+
+	var (
+		prompt = d.Config.Prompt
+		hide   = d.Config.HideInput
+	)
+	if useSpec {
+		spec := rs.Init(&inputSpec{}, nil).(*inputSpec)
+		err = yaml.Unmarshal(promptBytes, spec)
+		if err != nil {
+			return nil, fmt.Errorf("renderer.%s: invalid input spec %w", d.name, err)
+		}
+
+		if spec.Config.HideInput != nil {
+			hide = spec.Config.HideInput
+		}
+
+		if len(spec.Config.Prompt) != 0 {
+			prompt = spec.Config.Prompt
+		}
+	} else if len(promptBytes) != 0 {
+		prompt = string(promptBytes)
+	}
+
+	fmt.Print(prompt)
+
+	if hide != nil && *hide {
 		ret, err2 := term.ReadPassword(int(os.Stdin.Fd()))
 		fmt.Println()
 		return ret, err2
 	}
 
-	ret, err := readline(os.Stdin)
+	ret, err := iohelper.ReadInputLine(os.Stdin)
 	fmt.Println()
 	return ret, err
 }
